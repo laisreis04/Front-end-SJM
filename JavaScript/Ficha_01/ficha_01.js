e8ea1c12cf1e6c2cca3cf46b4b288919878eba69
Laís Reis <laisreis04@gmail.com>
2025-10-02 16:16:16 +0100

    Ficha 01

---
+++ JavaScript/Ficha_01/ficha_01.js
@@ -0,0 +1,29 @@
+//01
+
+// let frase = "Aprender Javascript é divertido!"
+// console.log(frase.length)
+
+
+//02
+// const myString = "Javascript é demais!"
+// console.log(`Na posição 0 está a letra ${myString[0]} \n e posição 10 ${myString[10]}`)
+
+//03
+// let texto = "A Laís é a melhor formadora do mundo"
+// console.log(texto.toUpperCase())
+// console.log(texto.toLowerCase())
+
+//04
+// let myString = "Eu adoro programar em JAVA!"
+// console.log(myString.replace("JAVA" , "Javascript"))
+
+//05
+//Declarar variavel
+// let minhaFrase = "A prática leva a perfeição"
+// console.log(minhaFrase.includes("prática"))
+// console.log(minhaFrase.includes("profissão"))
+
+//06
+// let frutasString = "banana, maça, uva, abacaxi";
+// let listaFrutas = frutasString.split(",");
+// console.log(listaFrutas)
---
+++ script.js
@@ -0,0 +1,19 @@
+// //Exercício 1
+
+// let myLuckyNumber = 15;
+
+// //2 variavel
+// let myName = 'Lais';
+
+// alert(myLuckyNumber + " " + myName)
+
+
+//Ex 02
+
+// const message = "       TASTES THE RAIWBOW!    ";
+
+// let whisper = message;
+
+// console.log(whisper.toLowerCase().trim())
+
+// Ex03
