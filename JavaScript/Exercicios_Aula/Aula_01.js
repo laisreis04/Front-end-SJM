b7765885a796388cd632f984d2b55f687c5da33b
Lais Reis <116274581+laisreis04@users.noreply.github.com>
2025-10-07 16:55:06 +0100

    Objetos

--- JavaScript/Exercicios_Aula/Aula_01.js
+++ JavaScript/Exercicios_Aula/Aula_01.js
@@ -20,3 +20,51 @@
 
 
 //PLANETAS
+// //Array de planetas
+// let planetas = ["Mercúrio", "Vénus", "Terra", "Mart", "Jupiter", "Saturno", "Urano", "Neptuno", "Plutão"];
+
+// //Corrigi a gaveta de indice 03
+// planetas[3] = "Marte"
+
+// // Retirar plutão
+// planetas.pop();
+
+// //adicionar um novo planeta no inicio do Array
+// planetas.unshift('Narnia');
+
+// console.log(planetas)
+
+//Inserir Nome
+
+// const airplaneSeats = [
+//    ['Ruth', 'Anthony', 'Stevie'],
+//    ['Amelia', 'Pedro', 'Maya'],
+//    ['Xavier', 'Ananya', 'Luis'],
+//    ['Luke', null, 'Deniz'],
+//    ['Rin', 'Sakura', 'Francisco']
+// ];
+
+// console.log(airplaneSeats)
+
+// airplaneSeats[3][1] = 'Lais'
+// console.log(airplaneSeats)
+
+
+//Criar objeto
+
+let product = {
+nome: 'banana',
+inStock: true,
+price:1.99,
+colors: ['vermelho', 'azul', 'verde']
+};
+
+
+console.log(product.price)
+console.log(product.colors[2])
+
+product.price = 2.55
+
+console.log(product.price)
+console.log(product)
+
