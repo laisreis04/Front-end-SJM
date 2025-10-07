e019d3adebc02e77f3230c5c06b55ea1c162ce86
Lais Reis <116274581+laisreis04@users.noreply.github.com>
2025-10-07 15:29:19 +0100

    ok

--- JavaScript/Exercicios_Aula/Aula_01.js
+++ JavaScript/Exercicios_Aula/Aula_01.js
@@ -1,19 +1,22 @@
-let weekDay = prompt('Qual o dia da semana?');
+// let weekDay = prompt('Qual o dia da semana?');
 
-let semanaCaixaAlta = weekDay.toLowerCase();
+// let semanaCaixaAlta = weekDay.toLowerCase();
 
-if(semanaCaixaAlta === 'segunda'){
-    alert('Segunda ... Buhhh')
-}
-if(semanaCaixaAlta === 'terça'){
-    alert('terça ... Buhhh')
-}
-if(semanaCaixaAlta === 'quarta'){
-    alert('Quarta ... Buhhh')
-}
-if(semanaCaixaAlta === 'quinta'){
-    alert('Quinta ... Buhhh')
-}
-if(semanaCaixaAlta === 'sexta'){
-    alert('Sextouuuu')
-}
+// if(semanaCaixaAlta === 'segunda'){
+//     alert('Segunda ... Buhhh')
+// }
+// if(semanaCaixaAlta === 'terça'){
+//     alert('terça ... Buhhh')
+// }
+// if(semanaCaixaAlta === 'quarta'){
+//     alert('Quarta ... Buhhh')
+// }
+// if(semanaCaixaAlta === 'quinta'){
+//     alert('Quinta ... Buhhh')
+// }
+// if(semanaCaixaAlta === 'sexta'){
+//     alert('Sextouuuu')
+// }
+
+
+//PLANETAS
