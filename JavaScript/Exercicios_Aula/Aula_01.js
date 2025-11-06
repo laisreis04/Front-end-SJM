c67b110229b414d42a6c31bb30c54e9352a084bb
Lais Reis <116274581+laisreis04@users.noreply.github.com>
2025-11-06 12:26:49 +0000

    Arrow

--- JavaScript/Exercicios_Aula/Aula_01.js
+++ JavaScript/Exercicios_Aula/Aula_01.js
@@ -197,12 +197,12 @@
 
 //All Evens
 
-allEvens([3,5,7,9,11]) //Apenas impares - False
-allEvens([2,4,6,8,10]) //Apenas pares  - True
-allEvens([1,2,3,4,5,6,7,8,9]) //Misto - Falso
+// allEvens([3,5,7,9,11]) //Apenas impares - False
+// allEvens([2,4,6,8,10]) //Apenas pares  - True
+// allEvens([1,2,3,4,5,6,7,8,9]) //Misto - Falso
 
-function allEvens(array){
-    return console.log(array.every(pares => pares % 2 === 0))
-}
+// function allEvens(array){
+//     return console.log(array.every(pares => pares % 2 === 0))
+// }
 
 
